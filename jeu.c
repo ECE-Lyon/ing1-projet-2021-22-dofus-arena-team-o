7a2ac7cc62fc0984dbffc69ddebccacd0319da3a
ilaydakocer <ilayda.kocer@edu.ece.fr>
2022-05-16 12:38:53 +0200

    convertions

--- jeu.c
+++ jeu.c
@@ -466,6 +466,8 @@ void afficherCaracteristiqueJoueur(Jeux jeu, InfoEcran ecran, int joueurQuiJoue,
 
 }
 
+
+void dessinerChrono ()
 /// Ordre aléatoire des joueurs
 
 int getRandomInteger(int min, int max){
